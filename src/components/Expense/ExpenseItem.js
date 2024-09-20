b984727984f32e065bba059f5dece504bbc6c463
vadlamudipradeep <penchalpradeep2000@gmail.com>
2024-09-20 17:56:38 +0530

    add delete btn

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -10,7 +10,7 @@ const AddExpense=(props)=>{
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.price}</div>
       </div>
-
+<button className="expense-item__delete-btn">DELETE</button>
     </Card>
    
     
